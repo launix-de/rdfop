958f53e331ef19b8a29dae98978f6570e5323a98
Carl-Philip Hänsch <carl-philip.haensch@dzastro.de>
2024-05-29 19:05:11 +0200

    allow <?rdf templating

--- lib/rdfop-parser.scm
+++ lib/rdfop-parser.scm
@@ -22,6 +22,7 @@ Copyright (C) 2024  Carl-Philip Hänsch
 	(parser '((define loop_header rdf_select) (atom "BEGIN" true) (define loop_body rdfhp_program) (atom "END" true)) '("loop" loop_header loop_body))
 	(parser (define select rdf_select) '("select" select))
 	(parser '((atom "PRINT" true) (define format (regex "[a-zA-Z0-9_]+")) (define value rdf_expression)) '("print" format value))
+	(parser '((atom "?>" true) (define value (regex "(?:[^<]+|<[^?])*")) (atom "<?rdf" false)) '("print" "RAW" value))
 )))
 (define rdfhp_program (parser '((define statements (* rdfhp_statement)) (atom "")) statements "^(?:/\\*.*?\\*/|--[^\r\n]*[\r\n]|--[^\r\n]*$|[\r\n\t ]+)+"))
 
@@ -32,8 +33,8 @@ Copyright (C) 2024  Carl-Philip Hänsch
 	/* TODO: parse RDFHP header with parameters */
 	(match (ttl_header template) '("prefixes" definitions "rest" body) (begin
 		(define compile (lambda (program context) (match program
-			(cons '("param" '('get_var sym) value) rest) '('!begin '('set sym '('('req "query") (rdf_replace_context value context))) (compile rest (append context sym sym)))
-			(cons '("print" format value) rest) '('!begin '('print '((coalesce (rdfhp_filters (toUpper format)) (error "print: unknown format filter: " format)) (rdf_replace_context value context))) (compile rest context))
+			(cons '("param" '('get_var sym) value) rest) '('!begin '('set sym '('('req "query") (rdf_replace_ctx value context))) (compile rest (append context sym sym)))
+			(cons '("print" format value) rest) '('!begin '('print '((coalesce (rdfhp_filters (toUpper format)) (error "print: unknown format filter: " format)) (rdf_replace_ctx value context))) (compile rest context))
 			(cons '("select" query) rest) '('!begin '('set 'o '('once '('lambda '('f) '('f)))) (rdf_queryplan schema query definitions context (lambda (cols context) '('o '('lambda '() (compile rest context))))))
 			(cons '("loop" query body) rest) '('begin '('set 'm '('mutex)) (rdf_queryplan schema query definitions context (lambda (cols context) '('m '('lambda '() (compile body context))))) (compile rest context))
 			(cons '("loop" query body else) rest) '('begin '('set 'm '('mutex)) '('set 'o '('once '('lambda '('result) '('if 'result (compile else context))))) (rdf_queryplan schema query definitions context (lambda (cols context) '('!begin '('o false) '('m '('lambda '() (compile body context)))))) '('o true) (compile rest context))
